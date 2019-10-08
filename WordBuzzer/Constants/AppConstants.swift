40d0fe6c41a52eacb242ccba2eb8b86fb9652df9
Mahendra Bhama <maahibhama@gmail.com>
2019-10-08 19:40:14 +0530

    Added AppConstants file

---
+++ WordBuzzer/Constants/AppConstants.swift
@@ -0,0 +1,16 @@
+//
+//  AppConstants.swift
+//  WordBuzzer
+//
+//  Created by Maahi Bhama  on 06/10/19.
+//  Copyright © 2019 Mahendra Bhama. All rights reserved.
+//
+
+import Foundation
+
+struct AppConstants {
+    static let FirstPlayer = "First Player"
+    static let SecondPlayer = "Second Player"
+    static let ThirdPlayer = "Third Player"
+    static let FourthPlayer = "Fourth Player"
+}
