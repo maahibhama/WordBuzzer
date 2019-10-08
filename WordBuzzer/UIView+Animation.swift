8332d684522939612946e17cbd66219074047890
Mahendra Bhama <maahibhama@gmail.com>
2019-10-08 19:37:49 +0530

    Added UIView+Animation

---
+++ WordBuzzer/UIView+Animation.swift
@@ -0,0 +1,23 @@
+//
+//  UIViewController+Animation.swift
+//  WordBuzzer
+//
+//  Created by Maahi Bhama  on 08/10/19.
+//  Copyright © 2019 Mahendra Bhama. All rights reserved.
+//
+
+import Foundation
+import UIKit
+ 
+extension UIView {
+    func fadeIn(_ duration: TimeInterval = 0.5, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
+        UIView.animate(withDuration: duration, delay: delay, options: UIView.AnimationOptions.curveEaseIn, animations: {
+            self.alpha = 1.0
+        }, completion: completion)  }
+    
+    func fadeOut(_ duration: TimeInterval = 0.5, delay: TimeInterval = 0.0, completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in}) {
+        UIView.animate(withDuration: duration, delay: delay, options: UIView.AnimationOptions.curveEaseIn, animations: {
+            self.alpha = 0.0
+        }, completion: completion)
+    }
+}
