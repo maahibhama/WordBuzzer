4a1ed97984d69fd164eea72affd88490f16dfc4a
Mahendra Bhama <maahibhama@gmail.com>
2019-10-10 21:37:43 +0530

    Implement Instruction table view cell with xib

---
+++ WordBuzzer/Views/InstructionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  InstructionTableViewCell.swift
+//  WordBuzzer
+//
+//  Created by Maahi Bhama  on 10/10/19.
+//  Copyright © 2019 Mahendra Bhama. All rights reserved.
+//
+
+import UIKit
+
+class InstructionTableViewCell: UITableViewCell {
+
+    //MARK:- Interface Builder Outlets
+    @IBOutlet weak var instructionLabel: UILabel!
+    
+    //MARK:- Nib Methods
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+}
